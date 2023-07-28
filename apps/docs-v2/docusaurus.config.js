42d8a19b36634cf0af4813b09700aabe162100ad
Eren Kulaksiz <erenkulaksz@gmail.com>
2023-07-28 23:21:14 +0300

    fix: fix issue path /intro

--- apps/docs-v2/docusaurus.config.js
+++ apps/docs-v2/docusaurus.config.js
@@ -87,7 +87,7 @@ const config = {
             items: [
               {
                 label: 'Get Started',
-                to: '/intro',
+                to: '/',
               },
             ],
           },
