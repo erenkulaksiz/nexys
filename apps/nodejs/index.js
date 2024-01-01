e67658295089f3289d6840b86eb024e1d5ba5620
Murat Kirazkaya <77299279+GroophyLifefor@users.noreply.github.com>
2024-01-01 19:04:53 +0300

    feat: some changes

--- apps/nodejs/index.js
+++ apps/nodejs/index.js
@@ -5,7 +5,7 @@ const nexys = new Nexys("API_KEY", { appName: "nodejsSample", debug: true })
 // simple log
 nexys.log("Info log.");
 
-// log with type
+// log with type info
 nexys.log(
   {
     name: 'steve',
--- packages/nexys/src/utils/guid.ts
+++ packages/nexys/src/utils/guid.ts
@@ -20,5 +20,6 @@ export function guid() {
       var p = (Math.random().toString(16)+"000000000").substr(2,8);
       return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
   }
+  
   return _p8() + _p8(true) + _p8(true) + _p8();
 }
